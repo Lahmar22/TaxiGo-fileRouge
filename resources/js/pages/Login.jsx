b3bedf4796dadff213afd2593146aceed91088b5
Lahmar22 <lahmarzakariae22@gmail.com>
2026-03-08 14:40:10 +0000

    feat : update login page

--- resources/js/pages/Login.jsx
+++ resources/js/pages/Login.jsx
@@ -1,3 +1,104 @@
+import logoImage from "../assets/TaxiGo.png";
+import { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+
 export default function Login() {
-    return <h1>Login Page</h1>;
+    const navigate = useNavigate();
+    const [form, setForm] = useState({
+        email: "",
+        password: "",
+    });
+    const [message, setMessage] = useState("");
+
+    const handleChange = (e) => {
+        setForm({
+            ...form,
+            [e.target.name]: e.target.value,
+        });
+    }; 
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        try {
+            const response = await axios.post("http://127.0.0.1:8000/api/login", form);
+            console.log(response.data.user)
+            const role = response.data.role;
+            if (response.data.success) {
+                if(role == "client"){
+                    navigate("/client/dashboard");
+                }else if(role == "admin"){
+                    navigate("/admin/dashboard");
+                }else if(role == "chauffeur"){
+                    navigate("/chauffeur/dashboard");
+                }else{
+                    navigate("/login");
+                }
+            }
+
+           
+        } catch (error) {
+            if (error.response) {
+                console.log(error.response.data);
+            }
+        }
+    };
+    return (
+        <div className="min-h-screen flex items-center justify-center bg-gradient-to-b from-white via-slate-50 to-slate-100 p-6">
+
+            <div className="w-full max-w-md bg-white shadow-xl rounded-3xl p-8 border border-slate-100">
+
+                <div className="flex justify-center mb-6">
+                    <img src={logoImage} alt="TaxiGo" className="h-16" />
+                </div>
+
+                <h2 className="text-2xl font-bold text-center text-slate-900 mb-6">
+                    Connexion
+                </h2>
+
+                <form className="space-y-5" onSubmit={handleSubmit}>
+
+                    {/* Email */}
+                    <div>
+                        <label className="block text-sm text-slate-600 mb-2">
+                            Email
+                        </label>
+                        <input
+                            type="email"
+                            name="email"
+                            placeholder="Votre email"
+                            onChange={handleChange}
+                            className="w-full px-4 py-3 rounded-xl border border-slate-200 focus:outline-none focus:ring-2 focus:ring-yellow-400 transition"
+                        />
+                    </div>
+
+                    {/* Password */}
+                    <div>
+                        <label className="block text-sm text-slate-600 mb-2">
+                            Mot de passe
+                        </label>
+                        <input
+                            type="password"
+                            name="password"
+                            placeholder="Votre mot de passe"
+                            onChange={handleChange}
+                            className="w-full px-4 py-3 rounded-xl border border-slate-200 focus:outline-none focus:ring-2 focus:ring-yellow-400 transition"
+                        />
+                    </div>
+
+                    {/* Button */}
+                    <button type="submit" className="w-full bg-gradient-to-r from-yellow-500 to-amber-600 text-white py-3 rounded-xl font-semibold hover:shadow-lg transition">
+                        Se connecter
+                    </button>
+
+                </form>
+                <div className="text-center mt-4 text-sm text-slate-600">
+                    Vous n'avez pas de compte ?{" "}
+                    <a href="/register" className="text-yellow-500 font-semibold hover:underline">
+                        S'inscrire
+                    </a>
+                </div>
+            </div>
+        </div>
+    );
 }
