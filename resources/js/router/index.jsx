c3a4329485ef0f804f06fc3268f636e46197a6a6
Lahmar22 <lahmarzakariae22@gmail.com>
2026-03-08 14:41:08 +0000

    feat : update route index

--- resources/js/router/index.jsx
+++ resources/js/router/index.jsx
@@ -1,6 +1,10 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "../pages/Home";
 import Login from "../pages/Login";
+import Register from "../pages/Register";
+import DashboardClient from "../pages/client/dashboard";
+import DashboardAdmin from "../pages/admin/Dashboard";
+import DashboardChauffeur from "../pages/chauffeur/Dashboard";
 
 export default function Router() {
     return (
@@ -8,6 +12,10 @@ export default function Router() {
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/login" element={<Login />} />
+                <Route path="/register" element={<Register/>} />
+                <Route path="/client/dashboard" element={<DashboardClient/>} />
+                <Route path="/admin/dashboard" element={<DashboardAdmin/>} />
+                <Route path="/chauffeur/dashboard" element={<DashboardChauffeur/>} />
             </Routes>
         </BrowserRouter>
     );
