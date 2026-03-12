df2206bffd23482d52a99e689e13dc927fdb30ff
Lahmar22 <lahmarzakariae22@gmail.com>
2026-03-12 09:31:12 +0000

    feat : update the client model

--- app/Models/Client.php
+++ app/Models/Client.php
@@ -3,11 +3,17 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Models\User;
 
 class Client extends Model
 {
     protected $fillable = [
         'user_id',
-
+        
     ];
+
+    public function client()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
