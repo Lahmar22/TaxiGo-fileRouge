9dc8a7a9324ba431a3ab066a5204be8be46c2d5d
Lahmar22 <lahmarzakariae22@gmail.com>
2026-03-16 09:38:28 +0000

    feat : update dashboard chauffeur

--- resources/js/pages/chauffeur/Dashboard.jsx
+++ resources/js/pages/chauffeur/Dashboard.jsx
@@ -1,7 +1,20 @@
+import { useState } from "react";
+import Header from "../chauffeur/Header";
+import Sidebar from "../chauffeur/Sidebar";
+
 export default function Dashboard() {
+    const [openSidebar, setOpenSidebar] = useState(false);
     return (
-        <>
-            <p>hello chauffeur</p>
-        </>
+        <div className="flex min-h-screen bg-slate-100">
+         <Sidebar open={openSidebar} setOpen={setOpenSidebar} />
+
+            <div className="flex flex-col flex-1">
+                <Header setOpenSidebar={setOpenSidebar} />
+
+                <main className="flex-1 p-6">
+                            <h1>yfjygjhgvj</h1>
+                </main>
+            </div>
+        </div>
     )
 }
