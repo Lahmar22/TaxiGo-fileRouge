695e4182154b53e751917b9ff3e871d29467146b
Lahmar22 <lahmarzakariae22@gmail.com>
2026-03-29 16:27:34 +0100

    feat : add the table for migration

---
+++ backend/database/migrations/2026_03_29_142846_create_courses_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('courses', function (Blueprint $table) {
+            $table->id();
+            $table->string('adresse_depart ');
+            $table->string('destination');
+            $table->double('distance ', 10, 2);
+            $table->double('prix_course ', 10, 2);
+            $table->string('status')->default('en attente');
+            $table->date('date_course ');
+            $table->foreignId('client_id')->constrained('clients')->onDelete('cascade');
+            $table->foreignId('chauffeur_id')->constrained('chauffeurs')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('courses');
+    }
+};
