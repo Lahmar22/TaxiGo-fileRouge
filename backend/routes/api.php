9d3f104f3993b083aad34126a5242bcd679b8e33
Lahmar22 <lahmarzakariae22@gmail.com>
2026-03-29 16:27:56 +0100

    feat : update route api

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\CourseController;
+use App\Http\Controllers\UserController;
 
 
 Route::post('/register', [AuthController::class, 'register']);
@@ -9,4 +11,7 @@ Route::post('/login', [AuthController::class, 'login']);
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
+    Route::apiResource('/courses', CourseController::class);
+    Route::get('/users', [UserController::class, 'index']);
+    Route::patch('/users/{id}', [UserController::class, 'banned']);
 });
