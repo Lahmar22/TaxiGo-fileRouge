c3315392cb11ec10db2f09bead0f43183e841239
Lahmar22 <lahmarzakariae22@gmail.com>
2026-03-30 10:16:00 +0100

    feat : update the user controller for banned the user

--- backend/app/Http/Controllers/UserController.php
+++ backend/app/Http/Controllers/UserController.php
@@ -18,12 +18,23 @@ class UserController extends Controller
     public function banned($id)
     {
         $user = User::findOrFail($id);
-        $user->banned = true;
+        $role = $user->role->role_name;
+
+        if ($role === 'admin') {
+           $message = 'Admin users cannot be banned';
+        } else {
+            if ($user->banned) {
+                $user->banned = false;
+                $message = 'User has been unbanned';
+            } else {
+                $user->banned = true;
+                $message = 'User has been banned';
+            }
+        }
         $user->save();
 
         return response()->json([
-            'message' => 'User banned successfully',
-            'user' => $user
+            'message' => $message
         ]);
     }
 }
