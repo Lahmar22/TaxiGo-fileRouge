ce606ffdb86a2a6a0ae1cb081728f16adf500ffb
Lahmar22 <lahmarzakariae22@gmail.com>
2026-03-30 10:17:47 +0100

    feat : update the model chauffeur for add relationShip

--- backend/app/Models/Chauffeur.php
+++ backend/app/Models/Chauffeur.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Models\User;
 
 class Chauffeur extends Model
 {
@@ -13,4 +14,9 @@ class Chauffeur extends Model
         'vehicule',
         'revenu_total',
     ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
