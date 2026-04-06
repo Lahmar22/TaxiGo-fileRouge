3d23585c531ccd89bff3d5201c929c54c1c490e3
Lahmar22 <lahmarzakariae22@gmail.com>
2026-04-06 16:22:34 +0100

    feat: add model vehicule

---
+++ backend/app/Models/Vehicule.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use App\Models\Chauffeur;
+
+class Vehicule extends Model
+{
+    protected $fillable = [
+        'permis',
+        'carte_grise',
+        'chauffeur_id',
+    ];
+
+    public function chauffeur()
+    {
+        return $this->belongsTo(Chauffeur::class);
+    }
+}
