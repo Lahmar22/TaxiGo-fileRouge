fbb4b316d58815df7807199c4eb6f93cd7688286
Lahmar22 <lahmarzakariae22@gmail.com>
2026-04-06 16:22:05 +0100

    feat: update the model chauffeur for rolationShip with vehicule

--- backend/app/Models/Chauffeur.php
+++ backend/app/Models/Chauffeur.php
@@ -4,19 +4,23 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use App\Models\User;
+use App\Models\Vehicule;
 
 class Chauffeur extends Model
 {
     protected $fillable = [
         'user_id',
         'status',
-        'number_permis',
-        'vehicule',
-        'revenu_total',
+        
     ];
 
     public function user()
     {
         return $this->belongsTo(User::class);
+    }
+
+    public function vehicule()
+    {
+        return $this->hasOne(Vehicule::class);
     }
 }
