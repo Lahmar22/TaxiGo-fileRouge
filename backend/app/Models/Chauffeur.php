6a427b2a4c85242f14616d64493b89a3bfc007ff
Lahmar22 <lahmarzakariae22@gmail.com>
2026-04-07 15:23:48 +0100

    feat: update model chauffeur for ralation with notification

--- backend/app/Models/Chauffeur.php
+++ backend/app/Models/Chauffeur.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use App\Models\User;
 use App\Models\Vehicule;
+use App\Models\Notification;
 
 class Chauffeur extends Model
 {
@@ -22,5 +23,10 @@ class Chauffeur extends Model
     public function vehicule()
     {
         return $this->hasOne(Vehicule::class);
+    }
+
+    public function notifications()
+    {
+        return $this->morphMany(Notification::class, 'notifiable');
     }
 }
