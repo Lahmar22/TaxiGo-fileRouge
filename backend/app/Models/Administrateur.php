b92c6118fa808b0a9d1c33f9765dd5b2f41bb9e6
Lahmar22 <lahmarzakariae22@gmail.com>
2026-04-07 15:21:30 +0100

    feat: update model admin for relation with user

--- backend/app/Models/Administrateur.php
+++ backend/app/Models/Administrateur.php
@@ -3,6 +3,8 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Models\User;
+use App\Models\Notification;
 
 class Administrateur extends Model
 {
@@ -10,4 +12,14 @@ class Administrateur extends Model
         'user_id',
 
     ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+     public function notifications()
+    {
+        return $this->morphMany(Notification::class, 'notifiable');
+    }
 }
