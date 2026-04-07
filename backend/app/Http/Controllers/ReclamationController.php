345f23a0c0c91677f1d6e122513ccf95cbd1c039
Lahmar22 <lahmarzakariae22@gmail.com>
2026-04-07 15:16:35 +0100

    feat: add controller reclamation for gere the reclamation the client

---
+++ backend/app/Http/Controllers/ReclamationController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Reclamation;
+use App\Http\Requests\CreateReclamationRequest;
+
+class ReclamationController extends Controller
+{
+    public function index()
+    {
+        $reclamations = Reclamation::with('client.user')->get(); 
+        return response()->json([
+            'reclamations' => $reclamations
+        ]);
+    }
+
+    public function store(CreateReclamationRequest $request)
+    {
+
+        $reclamation = Reclamation::create([
+            'user_id' => $request->user_id,
+            'description' => $request->description,
+            'date_reclamation' => now(),
+            'status' => 'en attente',
+        ]);
+
+        return response()->json([
+            'message' => 'Reclamation created successfully',
+            'reclamation' => $reclamation
+        ], 201);
+    }
+
+    public function updateStatus($id)
+    {
+        $reclamation = Reclamation::findOrFail($id);
+        $reclamation->status = 'résolue';
+        $reclamation->save();
+
+        return response()->json([
+            'message' => 'Reclamation status updated successfully',
+            'reclamation' => $reclamation
+        ]);
+    }
+}
+
+        
