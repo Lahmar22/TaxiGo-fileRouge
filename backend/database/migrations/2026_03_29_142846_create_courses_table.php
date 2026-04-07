8ef0e3f0e0568b8f633a69c0779f8133d8141ecb
Lahmar22 <lahmarzakariae22@gmail.com>
2026-04-07 21:15:28 +0100

    feat : update the table course in migration

--- backend/database/migrations/2026_03_29_142846_create_courses_table.php
+++ backend/database/migrations/2026_03_29_142846_create_courses_table.php
@@ -13,12 +13,12 @@ return new class extends Migration
     {
         Schema::create('courses', function (Blueprint $table) {
             $table->id();
-            $table->string('adresse_depart ');
+            $table->string('adresse_depart');
             $table->string('destination');
-            $table->double('distance ', 10, 2);
-            $table->double('prix_course ', 10, 2);
+            $table->double('distance', 10, 2);
+            $table->double('prix_course', 10, 2);
             $table->string('status')->default('en attente');
-            $table->date('date_course ');
+            $table->date('date_course');
             $table->foreignId('client_id')->constrained('clients')->onDelete('cascade');
             $table->foreignId('chauffeur_id')->constrained('chauffeurs')->onDelete('cascade');
             $table->timestamps();
