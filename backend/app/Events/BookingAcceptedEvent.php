d649c0fec30fa1d8f7cc880ca36f7c779be72fc1
Lahmar22 <lahmarzakariae22@gmail.com>
2026-04-15 16:56:23 +0100

    feat : add the rvents for booking accepte

---
+++ backend/app/Events/BookingAcceptedEvent.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Events;
+
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcastNow;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+use App\Models\Course;
+
+class BookingAcceptedEvent implements ShouldBroadcastNow
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    public function __construct(public Course $course) {}
+
+    public function broadcastOn(): Channel
+    {
+        return new Channel('courses');
+    }
+
+    public function broadcastAs(): string
+    {
+        return 'booking-accepted';
+    }
+
+    public function broadcastWith(): array
+    {
+        return [
+            'course' => [
+                'id' => $this->course->id,
+                'status' => $this->course->status,
+                'chauffeur_id' => $this->course->chauffeur_id,
+            ]
+        ];
+    }
+}
