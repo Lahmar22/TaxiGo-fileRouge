71bafe20d064e1ad093c234bf8bd557c9e6a5059
Lahmar22 <lahmarzakariae22@gmail.com>
2026-04-17 15:21:10 +0100

    feat : add paiemant request for valide the donnes

---
+++ backend/app/Http/Requests/CreatePaiementRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Foundation\Http\FormRequest;
+
+class CreatePaiementRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'montant' => 'required|numeric',
+            'mode_paiement' => 'required|string|in:carte,cash',
+            'payment_method_id' => 'required_if:mode_paiement,carte|string',
+            'status_paiement' => 'required|boolean',
+            'course_id' => 'required|exists:courses,id',
+        ];
+    }
+}
