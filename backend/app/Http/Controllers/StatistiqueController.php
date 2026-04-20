1ae30344ceb6115a8a8fa4e93baec8426777ba7f
Lahmar22 <lahmarzakariae22@gmail.com>
2026-04-20 16:53:51 +0100

    feat: upadate controller stats

--- backend/app/Http/Controllers/StatistiqueController.php
+++ backend/app/Http/Controllers/StatistiqueController.php
@@ -22,4 +22,32 @@ class StatistiqueController extends Controller
         ]);
 
     }
+
+    public function statisqtiqueChauffeur($id){
+        $totalCourse = Course::where('chauffeur_id', $id)->count();
+        $totalCourseConfirmer = Course::where('chauffeur_id', $id)->where('status', 'confirmee')->count();
+        $totalCourseTerminer = Course::where('chauffeur_id', $id)->where('status', 'terminee')->count();
+
+
+        return response()->json([
+            'totalCourse' => $totalCourse,
+            'totalCourseConfirmer' => $totalCourseConfirmer,
+            'totalCourseTerminer' => $totalCourseTerminer,
+        ]);
+    }
+
+    public function revenuChauffeur($id){
+
+        $revenu = Course::where('chauffeur_id', $id)
+            ->where('status', 'terminee')
+            ->join('paiements', 'courses.id', '=', 'paiements.course_id')
+            ->where('paiements.status_paiement', true)
+            ->sum('paiements.montant');
+        
+
+        return response()->json([
+            'revenu' => $revenu
+        ]);
+    }
 }
+
