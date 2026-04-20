042554bfefafec4da476bc49a6200e28246e59aa
Lahmar22 <lahmarzakariae22@gmail.com>
2026-04-20 16:56:08 +0100

    feat: update route epi

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -9,6 +9,7 @@ use App\Http\Controllers\StatistiqueController;
 use App\Http\Controllers\ReclamationController;
 use App\Http\Controllers\NotificationController;
 use App\Http\Controllers\PaiementController;
+use App\Http\Controllers\EvaluationController;
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
@@ -19,6 +20,10 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::get('/courses/available/offers', [CourseController::class, 'availableOffers']);
     Route::patch('/course/{id}/accepte', [CourseController::class, 'accepteOffre']);
     Route::patch('/course/{course}/annuler', [CourseController::class, 'annulerOffre']);
+    Route::patch('/course/{course}/terminer', [CourseController::class, 'terminerCourse']);
+    Route::post('/courses/{course}/rate', [CourseController::class, 'rateCourse']);
+    Route::get('/courses/{id}', [CourseController::class, 'show']);
+    Route::get('/courses/chauffeur/{id}', [CourseController::class, 'showByChauffeur']);
     Route::get('/users', [UserController::class, 'index']);
     Route::get('/user/{id}', [UserController::class, 'show']);
     Route::patch('/users/{id}', [UserController::class, 'banned']);
@@ -26,6 +31,8 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::patch('/chauffeur/{id}', [ChauffeurController::class, 'updateStatus']);
     Route::patch('/chauffeurs/{id}', [ChauffeurController::class, 'validateChauffeur']);
     Route::get('/statistiques', [StatistiqueController::class, 'index']);
+    Route::get('/statistiques/chauffeur/{id}', [StatistiqueController::class, 'statisqtiqueChauffeur']);
+    Route::get('/statistiques/chauffeur/{id}/revenu', [StatistiqueController::class, 'revenuChauffeur']);
     Route::get('/reclamations', [ReclamationController::class, 'index']);
     Route::post('/reclamations', [ReclamationController::class, 'store']);
     Route::patch('/reclamations/{id}', [ReclamationController::class, 'updateStatus']);
@@ -35,4 +42,7 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::post('/paiements', [PaiementController::class, 'store']);
     Route::post('/create-payment-intent', [PaiementController::class, 'createPaymentIntent']);
     Route::post('/confirm-payment', [PaiementController::class, 'confirmPayment']);
+
+    Route::post('/evaluations', [EvaluationController::class, 'store']);
+
 });
